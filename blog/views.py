be9d70c3f6243b15c87361f1d04b1daeabe91bca
miroslav <radanovic.miroslav@gmail.com>
2017-03-10 12:35:12 +0100

    added some base css and html files

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def main_view(request):
-    return render(request, 'blog/main_view.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/main_view.html', {'posts': posts})
