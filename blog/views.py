5d23afb9c424631936e935501696e8e3f1cf5755
miroslav <radanovic.miroslav@gmail.com>
2017-03-14 14:18:22 +0100

    added function for moving feedback notes

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post, Feedback
+from django.core import serializers
 
 def main_view(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     feedbacks = Feedback.objects.all()
-    return render(request, 'blog/main_view.html', {'posts': posts, 'feedbacks': feedbacks})
+    data = serializers.serialize('json', list(feedbacks), fields=('text','id', 'created_date', 'x_position', 'y_position', 'active'))
+
+    #return render(request, "blog/main_view.html", {'posts': posts, 'js_feedbacks': data})
+    return render(request, 'blog/main_view.html', {'posts': posts, 'feedbacks': feedbacks, 'js_feedbacks': data})
