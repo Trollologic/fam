a4fa6a12f2f00c4e512c9e9756351de03703944d
miroslav <radanovic.miroslav@gmail.com>
2017-03-15 16:04:59 +0100

    added function for posting new feedback

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_view, name='main_view'),
+    url(r'^test/', views.PostExample.as_view(), name='test-start'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post, Feedback
 from django.core import serializers
+from django.views.generic import TemplateView
+
+
+import json
 
 def main_view(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +14,20 @@ def main_view(request):
 
     #return render(request, "blog/main_view.html", {'posts': posts, 'js_feedbacks': data})
     return render(request, 'blog/main_view.html', {'posts': posts, 'feedbacks': feedbacks, 'js_feedbacks': data})
+
+
+class PostExample(TemplateView):
+    template_name = 'blog/main_view.html'
+    def post(self, request):
+        #body_unicode = request.body.decode('utf-8')
+        #body = json.loads(body_unicode)
+        #print 'Raw Data: "%s"' % request.POST['text']
+        Feedback(text=request.POST['text'], x_position= request.POST['x_position'], y_position= request.POST['y_position'], active=True).save()
+        
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        feedbacks = Feedback.objects.all()
+        print feedbacks
+        data = serializers.serialize('json', list(feedbacks), fields=('text','id', 'created_date', 'x_position', 'y_position', 'active'))
+
+        #return render(request, "blog/main_view.html", {'posts': posts, 'js_feedbacks': data})
+        return render(request, 'blog/main_view.html', {'posts': posts, 'feedbacks': feedbacks, 'js_feedbacks': data})
